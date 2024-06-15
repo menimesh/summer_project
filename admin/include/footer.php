992349b0f19c5fab580436dff786b774e75b80f0
menimesh <nimeshkhatiwadaa@gmail.com>
2024-06-15 14:51:11 +0545

    initial commit

---
+++ admin/include/footer.php
@@ -0,0 +1,1 @@
+ <footer class="footer" style="text-align: center;"> &#169menimesh </footer>
---
+++ include/footer.php
@@ -0,0 +1,38 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/card1.png" alt="esewa" height="30px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/mastercard.png" alt="Mastercard" height="30px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/card2.png" alt="khalti" height="30px"> </a>
+                              </li>
+                              <li>
+                                  <a href="#"> <img src="images/card3.png" alt="imepay" height="30px"> </a>
+                              </li>
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>Arjundhara,Jhapa</p>
+                          <h5>Phone: +977-9800000000</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Addition informations</h5>
+                          <p>Delivering happiness more than 3500+ customer.</p>
+                          <span>&copy</span>Menimesh
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
